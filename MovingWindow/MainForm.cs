d68798ee923bd73ee2d53f371831c1ffaaa14630
VitaliiRogozhyn <vitalii.rogozhy@gmail.com>
2018-06-05 19:39:27 +0300

    private void MoveForm() added, timer changed

--- MovingWindow/MainForm.cs
+++ MovingWindow/MainForm.cs
@@ -26,16 +26,32 @@ namespace MovingWindow
 
         private void TimerMoving_Tick(object sender, EventArgs e)
         {
+            MoveForm();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            centerLeftTop = new Point(Left, Top);
+            leftTop = new Point(Left, Top);
+            workingArea = Screen.GetWorkingArea(this);
+            int shiftStep = 6;
+            _command.Add(Keys.Left, new MoveLeft(leftTop, shiftStep));
+            _command.Add(Keys.Right, new MoveRight(leftTop, shiftStep));
+            _command.Add(Keys.Up, new MoveUp(leftTop, shiftStep));
+            _command.Add(Keys.Down, new MoveDown(leftTop, shiftStep));
+            _command.Add(Keys.Enter, new MoveCenter(leftTop, centerLeftTop));
+        }
+        private void MoveForm()
+        {
             _command[pressedKey].Execute();
             if (leftTop.X < 0)
             {
                 pressedKey = Keys.Right;
                 leftTop.X = 0;
             }
-            if (leftTop.X+Width > workingArea.Width)
+            if (leftTop.X + Width > workingArea.Width)
             {
                 pressedKey = Keys.Left;
-                leftTop.X = workingArea.Width-Width;
+                leftTop.X = workingArea.Width - Width;
             }
             if (leftTop.Y < 0)
             {
@@ -49,19 +65,6 @@ namespace MovingWindow
             }
             Left = leftTop.X;
             Top = leftTop.Y;
-        }
-
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-            centerLeftTop = new Point(Left, Top);
-            leftTop = new Point(Left, Top);
-            workingArea = Screen.GetWorkingArea(this);
-            int shiftStep = 6;
-            _command.Add(Keys.Left, new MoveLeft(leftTop, shiftStep));
-            _command.Add(Keys.Right, new MoveRight(leftTop, shiftStep));
-            _command.Add(Keys.Up, new MoveUp(leftTop, shiftStep));
-            _command.Add(Keys.Down, new MoveDown(leftTop, shiftStep));
-            _command.Add(Keys.Enter, new MoveCenter(leftTop, centerLeftTop));
         }
     }  
 }
