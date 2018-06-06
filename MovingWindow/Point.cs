77fd42b8830df5dd8291fa4fa862ed1d65e8bcc5
VitaliiRogozhyn <vitalii.rogozhy@gmail.com>
2018-06-06 19:43:50 +0300

    Close #6

--- MovingWindow/Point.cs
+++ MovingWindow/Point.cs
@@ -4,12 +4,12 @@
     {
         private int x;
         private int y;
-        public int X { get { return x; } set { x = value; } }
-        public int Y { get { return y; } set { y = value; } }
         public Point(int x, int y)
         {
             this.X = x;
             this.Y = y;
         }
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
     }
 }
